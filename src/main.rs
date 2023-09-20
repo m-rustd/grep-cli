be13e82de27dc334d6c6dd700c9a76ac5a2c94fc
zhouwei <zhouwei1@cheyipai.com>
2023-09-20 14:26:00 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use std::{env, process};
+use grep_cli::{Config, run};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    if let Err(e) = run(config) {
+      eprintln!("Application error: {}", e);
+      process::exit(1);
+    }
+}
